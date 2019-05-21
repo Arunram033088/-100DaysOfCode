e30134e944b55db3717e908a61911a407c30aa21
Arun Ramanathan <arunramanathan3@gmail.com>
2019-05-21 19:11:28 +0530

    Create learning_variable.py

---
+++ learning_variable.py
@@ -0,0 +1,4 @@
+# Assigning value for variable mv_population and updated the value of the variable with the assignment operators
+mv_population = 74728
+mv_population = mv_population + 4000 - 600
+print(mv_population)
